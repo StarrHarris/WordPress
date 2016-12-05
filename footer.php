afbfb49e24e8f5e3ad402b9530de3abea32a24eb
StarrHarris <sharri29@seattlecentral.edu>
2016-12-04 17:31:44 -0800

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,26 +1,18 @@
 <?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package lastchance
- */
 
-?>
 
-	</div><!-- #content -->
+	</div>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<p>&copy; <?php echo date('Y');?> <a href="<?php bloginfo('url');?>">Starr Harris</a>. All rights reserved.</p>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</div>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
 </body>
 </html>
+
+?>
